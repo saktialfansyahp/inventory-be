c331ae22ca25d522681dd4c596685885d0fb7366
saktialfansyahp <saktialfansyahp@gmail.com>
2023-11-06 01:35:20 +0700

    cors

--- api/index.php
+++ api/index.php
@@ -12,4 +12,18 @@ $response = $kernel->handle(
     $request = Request::capture()
 );
 $response->send();
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    $kernel->terminate($request, $response);
+    exit;
+}
+
 $kernel->terminate($request, $response);
--- public/index.php
+++ public/index.php
@@ -52,17 +52,4 @@ $response = $kernel->handle(
     $request = Request::capture()
 )->send();
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    $kernel->terminate($request, $response);
-    exit;
-}
-
 $kernel->terminate($request, $response);
