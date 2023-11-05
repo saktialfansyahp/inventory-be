aa2d277f509b8a110c4c2fbd062f20f397872f88
saktialfansyahp <saktialfansyahp@gmail.com>
2023-11-06 01:58:41 +0700

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,4 +52,11 @@ $app->singleton(
 |
 */
 
+$app->after(function ($request, $response) {
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+    $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+});
+
+
 return $app;
