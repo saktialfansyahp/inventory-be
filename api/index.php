e8fbf6be7dc94d7bc250648310b32d2ca42b5f47
saktialfansyahp <saktialfansyahp@gmail.com>
2023-11-06 21:59:20 +0700

    pliss

--- api/index.php
+++ api/index.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Http\Kernel;
 
 // Load the Laravel application
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Run the application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make(Kernel::class);
 $response = $kernel->handle(
     $request = Request::capture()
 );
